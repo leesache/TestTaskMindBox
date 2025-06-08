3903e656a763bca7bf000936484fa76afead193a
leesache <kwartiranowaja@gmail.com>
2025-06-08 21:10:22 +0300

    [AC-1] Edited README.md, setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="area_calculator",
+    name="area_calculator_mindbox_testtask",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],
